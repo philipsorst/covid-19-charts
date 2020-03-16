7e7c376dc2bdb95e94ac803059dbd8837ba95d46
Philip Washington Sorst <philip@sorst.net>
2020-03-16 22:24:59 +0100

    Massively improving map

--- src/js/map.ts
+++ src/js/map.ts
@@ -3,6 +3,7 @@ import * as d3 from 'd3';
 import * as TopoJsonClient from 'topojson-client';
 import {CountryData} from "./country-data";
 import {CovidData} from "./covid-data";
+import {DayData} from "./day-data";
 
 require('../scss/map.scss');
 
@@ -11,61 +12,120 @@ let container = d3.select("#map");
 const containerWidth = 1000;
 const svg = container.append("svg")
     .attr('width', containerWidth)
-    .attr('height', 1000);
+    .attr('height', 500);
 const projection = d3.geoNaturalEarth1();
 console.log(projection.scale());
 const path = d3.geoPath().projection(projection);
-// const color = d3.scaleOrdinal<number, string>(d3.schemeCategory10);
-let countryData: CountryData;
-let covidData: CovidData;
+let paths;
 
-CountryData.load().then((countryDataResult) => {
-    countryData = countryDataResult;
-    CovidData.load(countryData).then(covidDataResult => {
-        covidData = covidDataResult;
+interface MapPlot
+{
+    name: string,
+    range: ReadonlyArray<string>,
+    data: (countryCode: string, dayData: DayData, covidData: CovidData, countryData: CountryData) => number
+}
 
-        let maxPendingRate = 0;
-        covidData.getCountryCodes().forEach(countryCode => {
-            const lastEntry = covidData.getLastDayData(countryCode);
-            const population = countryData.getPopulation(countryCode);
-            if (null != population) {
-                const pendingRate = lastEntry.getPending() / population;
-                if (pendingRate > maxPendingRate) {
-                    maxPendingRate = pendingRate;
-                    console.log(maxPendingRate, countryCode);
-                }
+const pendingPercentagePlot: MapPlot = {
+    name: 'Pending Percentage',
+    range: ['#dedede', '#ff0000'],
+    data: (countryCode: string, dayData, covidData, countryData) => {
+        const population = countryData.getPopulation(countryCode);
+        if (null == population) {
+            return 0;
+        }
+        return dayData.getPending() / population;
+    }
+};
+
+const deathRatePlot: MapPlot = {
+    name: 'Death Rate',
+    range: ['#dedede', '#ff0000'],
+    data: (countryCode: string, dayData, covidData, countryData) => {
+        return dayData.getDeathRate();
+    }
+};
+
+const growthRatePlot: MapPlot = {
+    name: 'Growth Rate',
+    range: ['#dedede', '#ff0000'],
+    data: (countryCode: string, dayData, covidData, countryData) => {
+        return dayData.growthRate;
+    }
+};
+
+const confirmedPlot: MapPlot = {
+    name: 'Confirmed',
+    range: ['#dedede', '#ff0000'],
+    data: (countryCode: string, dayData, covidData, countryData) => {
+        return dayData.confirmed;
+    }
+};
+
+const recoveredPlot: MapPlot = {
+    name: 'Recovered',
+    range: ['#dedede', '#80ff80'],
+    data: (countryCode: string, dayData, covidData, countryData) => {
+        return dayData.recovered;
+    }
+};
+
+const recoveredPercentagePlot: MapPlot = {
+    name: 'Recovered',
+    range: ['#dedede', '#80ff80'],
+    data: (countryCode: string, dayData, covidData, countryData) => {
+        return dayData.recovered / dayData.confirmed;
+    }
+};
+
+function plotMap(
+    covidData: CovidData,
+    countryData: CountryData,
+    features: Array<GeoJSON.Feature<GeoJSON.GeometryObject, {}>>,
+    mapPlot: MapPlot)
+{
+    const extents = new Array<[number, number]>();
+    features.forEach(feature => {
+        const countryName = (feature.properties as { name: string }).name;
+        const countryCode = countryData.getCountryCode(countryName);
+        if (covidData.hasCountryCode(countryCode)) {
+            const dayData = covidData.getDayData(countryCode);
+            extents.push(d3.extent(dayData, d => mapPlot.data(countryCode, d, covidData, countryData)) as [number, number]);
+        }
+    });
+    const extent = [d3.min(extents, d => d[0]) as number, d3.max(extents, d => d[1]) as number];
+
+    const color = d3.scaleLinear<string, string>().domain(extent).range(mapPlot.range);
+    paths = svg
+        .selectAll('path')
+        .data(features)
+        .transition()
+        .duration(500)
+        .attr('fill', d => {
+            const countryName = (d.properties as { name: string }).name;
+            const countryCode = countryData.getCountryCode(countryName);
+            if (!covidData.hasCountryCode(countryCode)) {
+                return color(extent[0]);
             }
+
+            return color(mapPlot.data(countryCode, covidData.getLastDayData(countryCode), covidData, countryData));
         });
-        console.log(maxPendingRate);
 
-        const color = d3.scaleSymlog<string, string>().domain([0, maxPendingRate]).range(['#dedede', '#ff0000']);
+}
 
-        d3.json('./build/world-atlas/countries-110m.json').then(world => {
-            console.log(world);
-            let features = TopoJsonClient.feature(world, world.objects.countries as GeometryCollection).features;
-            console.log(features);
-            svg.selectAll("path")
+CountryData.load().then((countryData) => {
+    CovidData.load(countryData).then(covidData => {
+        d3.json('./build/world-atlas/countries-110m.json').then(worldData => {
+            let features = TopoJsonClient.feature(worldData, worldData.objects.countries as GeometryCollection).features;
+            svg
+                .selectAll('path')
                 .data(features)
                 .enter()
                 .append('path')
                 .attr('d', path)
-                .attr('fill', d => {
-                    const countryName = (d.properties as { name: string }).name;
-                    const countryCode = countryData.getCountryCode(countryName);
-                    if (!covidData.hasCountryCode(countryCode)) {
-                        return color(0);
-                    }//throw 'Country map not found for ' + countryCode;
-                    const population = countryData.getPopulation(countryCode);
-                    if (null == population) {
-                        return color(0);
-                    }
-                    const lastEntry = covidData.getLastDayData(countryCode);
-                    const pendingRate = lastEntry.getPending() / population;
+                .attr('stroke', '#808080')
+                .attr('fill', '#dedede');
 
-                    return color(pendingRate);
-                });
+            plotMap(covidData, countryData, features, pendingPercentagePlot);
         });
-
-    })
+    });
 });
-
