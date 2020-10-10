d28b0d9591bae05fc6a3b33bc93803ae4c76134f
Philip Washington Sorst <philip@sorst.net>
2020-10-10 20:55:21 +0200

    Adding dashboard component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
+import {DashboardComponent} from "./dashboard.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: ':countryCode',
+        component: DashboardComponent
+    },
+    {
+        path: '',
+        component: DashboardComponent,
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
