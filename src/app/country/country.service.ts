bc11b574af821c9172e97ba0bc05eb5ec0bd2bc5
Philip Washington Sorst <philip@sorst.net>
2020-10-11 16:36:06 +0200

    Refactoring country (wip)

---
+++ src/app/country/country.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+
+@Injectable({providedIn: 'root'})
+export class CountryService
+{
+
+    public list(): Observable<any>
+    {
+
+    }
+}
--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -1,9 +1,21 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {CountryService} from "./country.service";
 
 @Component({
     templateUrl: './dashboard.component.html',
     styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent
+export class DashboardComponent implements OnInit
 {
+    constructor(private countryService: CountryService)
+    {
+    }
+
+    /**
+     * @override
+     */
+    public ngOnInit()
+    {
+        this.countries$ = this.countryService.list();
+    }
 }
